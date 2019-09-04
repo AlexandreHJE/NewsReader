79217e6b4c39e5c85b70e1655151d9d0d94ffa15
Alex Hu <alexandre.hujenen@gmail.com>
2019-09-05 01:53:01 +0800

    Add LoadingScreenView file

---
+++ NewsReader/CustomUI/LoadingScreenView/LoadingScreenView.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingScreenView.swift
+//  NewsReader
+//
+//  Created by 胡仁恩 on 2019/9/4.
+//  Copyright © 2019 alexHu. All rights reserved.
+//
+
+import UIKit
+
+class LoadingScreenView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
